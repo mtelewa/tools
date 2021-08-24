6397e33acabee82dfe4c9d937fb95a393913a1eb
mtelewa <mohamed.hassan@kit.edu>
2021-08-24 18:08:03 +0200

    added help function and comments

--- md/post_proc.sh
+++ md/post_proc.sh
@@ -1,24 +1,50 @@
 #! /bin/sh
 
-usage="$(basename "$0") [-h -f] [-N] -- program to calculate the answer to life, the universe and everything
+usage="$(basename "$0") [-h] [-i -N] -- run the netcdf post-processor
 
 where:
     -h  show this help text
-    -f  trajectory file
-    -N  set the No. of chunks (default: 144)"
- 
-#Nchunks=144
-#infile="file"
+    -i  trajectory file
+    -N  set the No. of chunks
+    -f    Fluid
+    -s   stable start
+    -e   stable end
+    -p   pump start
+    -x   pump end"
 
-while getopts ':hfN:' option; do
+# Flags ----------------
+# This is a while loop that uses the getopts function and a so-called optstring
+# to iterate through the arguments.
+# The while loop walks through the optstring, which contains the flags that are
+# used to pass arguments and assigns the argument value provided for that flag to
+# the variable option.
+# The case statement then assigns the value of the variable option to a global
+# variable that is used after all the arguments have been read.
+
+#The colons in the optstring mean that values are required for the corresponding flags.
+#In the above example of u:d:p:f:, all flags are followed by a colon.
+#This means all flags need a value. If, for example,
+#the d and f flags were not expected to have a value, u:dp:f would be the optstring.
+
+while getopts ":h:i:N:f:s:e:p:x:" option; do
   case "$option" in
     h) echo "$usage"
        exit
        ;;
-    f) infile=$OPTARG
-       ;;
-    N) Nchunks=$OPTARG
-       ;;   
+    i) infile="$OPTARG"
+    ;;
+    N) Nchunks="$OPTARG"
+    ;;
+    f) fluid="$OPTARG"
+    ;;
+    s) stable_start="$OPTARG"
+    ;;
+    e) stable_end="$OPTARG"
+    ;;
+    p) pump_start="$OPTARG"
+    ;;
+    x) pump_end="$OPTARG"
+    ;;
     :) printf "missing argument for -%s\n" "$OPTARG" >&2
        echo "$usage" >&2
        exit 1
@@ -29,15 +55,17 @@ while getopts ':hfN:' option; do
        ;;
   esac
 done
+# call the shift command at the end of the processing loop to remove options that
+# have already been handled from $@
 shift $((OPTIND - 1))
 
-echo $infile
+mpirun -np 8 proc.py $infile.nc $Nchunks 1 1000 $fluid $stable_start $stable_end $pump_start $pump_end
+mpirun -np 8 proc.py $infile.nc 1 $Nchunks 1000 $fluid $stable_start $stable_end $pump_start $pump_end
 
-mpirun -np 8 postproc.py $1.nc 144 1 1000 $2 ${3:-1}
-cdo mergetime $1_144x1_*.nc $1_144x1.nc ; rm $1_144x1_*
-
-mpirun -np 8 postproc.py $1.nc 1 144 1000 $2 ${3:-1}
-cdo mergetime $1_1x144_*.nc $1_1x144.nc ; rm $1_1x144_*
-
-#mpirun -np 8 mpi_netcdf_postproc.py $1.nc 36 1 1000 $2 ${3:-1}
-#cdo merge $1_36x1_*.nc $1_36x1.nc ; rm $1_36x1_*
+if [ ! -f ${infile}_${Nchunks}x1_001.nc ]; then
+  mv ${infile}_${Nchunks}x1_000.nc ${infile}_${Nchunks}x1.nc
+  mv ${infile}_1x${Nchunks}_000.nc ${infile}_1x${Nchunks}.nc
+else
+  cdo mergetime $infile_${Nchunks}x1_*.nc $infile_${Nchunks}x1.nc ; rm $infile_${Nchunks}x1_*
+  cdo mergetime ${infile}_1x${Nchunks}_*.nc ${infile}_1x${Nchunks}.nc ; rm ${infile}_1x${Nchunks}"_*
+fi
