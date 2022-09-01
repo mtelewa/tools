3b8bb7f4890cd4058b7a4c603f6137ffaeea7b3a
mtelewa <mohamed.hassan@kit.edu>
2022-09-01 12:23:15 +0200

    renamed processor

--- md/grid.py
+++ md/grid.py
@@ -7,7 +7,7 @@ import scipy.constants as sci
 import time as timer
 from mpi4py import MPI
 import netCDF4
-import processor_nc as pnc
+import processor_nc_220901 as pnc
 from operator import itemgetter
 import re
 
