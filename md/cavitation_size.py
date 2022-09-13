cbf67ee1c8fbf045c107205f97814916ebac4c77
mtelewa <mohamed.hassan@kit.edu>
2022-09-13 13:21:44 +0200

    compute cavitation radius, area and volume from Ovito surface mesh modifier

--- md/cavitation_size.py
+++ md/cavitation_size.py
@@ -4,22 +4,10 @@
 import sys, os
 import numpy as np
 import pandas as pd
+import progressbar 
 from ovito.data import DataTable
 from ovito.io import import_file, export_file
 from ovito.modifiers import ConstructSurfaceModifier, TimeSeriesModifier
-
-# def progressbarstr(x, maxx, len=60):
-#     xx = x/maxx
-#     if xx != 1:
-#       return '|'+int(xx*len)*'#'+((len-int(xx*len))*'-')+'| {:>3}% ' \
-#              '({}/{})'.format(int(xx*100), x, maxx)
-#     else:
-#       return '|'+int(xx*len)*'#'+((len-int(xx*len))*'-')+'| {:>3}% ' \
-#              '({}/{})\n'.format(int(xx*100), x, maxx)
-#
-# def progressbar(x, maxx, len=60):
-#     sys.stdout.write('{}\r'.format(progressbarstr(x, maxx, len=len)))
-#     sys.stdout.flush()
 
 # Import the trajectory
 pipeline = import_file("flow.nc", multiple_frames = True)
