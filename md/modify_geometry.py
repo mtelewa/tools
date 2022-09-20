0d25ef872778bd7b39853edb7a1038f80d2c4766
mtelewa <mohamed.hassan@kit.edu>
2022-09-20 11:24:24 +0200

    REFACTOR: extended the method's arguments

--- md/modify_geometry.py
+++ md/modify_geometry.py
@@ -5,7 +5,7 @@ import sys
 from ovito.io import import_file, export_file
 from ovito.modifiers import ExpressionSelectionModifier, DeleteSelectedModifier
 
-def modify_geometry(datafile, slope, intercept):
+def modify_geometry(datafile, slope, intercept, cut_begin):
     """
     Import the NetCDF trajectory and add the construct surfacr mesh modifier
 
@@ -13,6 +13,7 @@ def modify_geometry(datafile, slope, intercept):
     datafile: str, LAMMPS data file to modify
     slope: float, slope of the cut plane
     intercept: float, shift the cut plane along the z-direction
+    cut_begin: float, x-coordinate to use in the boolean expression for removing atoms
     """
 
     pipeline = import_file(datafile, multiple_frames = False)
@@ -28,15 +29,15 @@ def modify_geometry(datafile, slope, intercept):
 
     # Select atoms from the lower wall above the slope
     pipeline.modifiers.append(ExpressionSelectionModifier(
-        expression = f'Position.X>0.6*CellSize.X && Position.Z<0.5*CellSize.Z \
+        expression = f'Position.X>{cut_begin}*CellSize.X && Position.Z<0.5*CellSize.Z \
                       && Position.Z>({slope}*Position.X)+{intercept}'))
 
     # Delete the selected atoms
     pipeline.modifiers.append(DeleteSelectedModifier())
 
-    # Select atoms from the upper wall below the slope
+    # Select atoms from the upper wall below the slope # the 3 used here is one unit length in the z-direction for gold (sqrt(3)*4.08)
     pipeline.modifiers.append(ExpressionSelectionModifier(
-        expression = f'Position.X>0.6*CellSize.X && Position.Z>0.5*CellSize.Z \
+        expression = f'Position.X>{cut_begin}*CellSize.X && Position.Z>0.5*CellSize.Z \
                       && Position.Z<({-slope}*Position.X)+{-intercept-3}+CellSize.Z'))
 
     pipeline.modifiers.append(DeleteSelectedModifier())
@@ -46,6 +47,9 @@ def modify_geometry(datafile, slope, intercept):
 
 
 if __name__ == "__main__":
+    # For iter-14
+    # pipeline = modify_geometry(sys.argv[1], 0.186, -64, 0.6)
+    # For iter-16
+    pipeline = modify_geometry(sys.argv[1], 0.884, -475, 0.9)
 
-    pipeline = modify_geometry(sys.argv[1], 0.186, -64) # For iter-14
     export_file(pipeline, "output.data", "lammps/data", atom_style="molecular")
