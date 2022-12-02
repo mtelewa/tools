6229918574e595413152a33cf85f09475fd91c75
mtelewa <mohamed.hassan@kit.edu>
2022-12-02 15:29:22 +0100

    deprecate the use of info.data

--- md/processor_profile.py
+++ md/processor_profile.py
@@ -23,20 +23,22 @@ class profile:
     def __init__(self,inputfile,tSkip,columns):
 
         # ------------------- Collecting the data -------------------------------#
-        with open('info.dat', 'r') as f:
-            for line in f:
-                if line.split()[0]=='BoxHeight':
-                    total_box_height=float(line.split()[1])
-                if line.split()[0]=='gapHeight':
-                    gapheight=float(line.split()[1])
-                if line.split()[0]=='AChunk':
-                    ChunkArea=float(line.split()[1])
-                if line.split()[0]=='VChunkX':
-                    ChunkVolX=float(line.split()[1])
-                if line.split()[0]=='VChunkZ':
-                    ChunkVolZ=float(line.split()[1])
-                if line.split()[0]=='vShear':
-                    vwall=float(line.split()[1])
+        # with open('info.dat', 'r') as f:
+        #     for line in f:
+        #         if line.split()[0]=='BoxHeight':
+        #             total_box_height=float(line.split()[1])
+        #         if line.split()[0]=='gapHeight':
+        #             gapheight=float(line.split()[1])
+        #         if line.split()[0]=='AChunk':
+        #             ChunkArea=float(line.split()[1])
+        #         if line.split()[0]=='VChunkX':
+        #             ChunkVolX=float(line.split()[1])
+        #         if line.split()[0]=='VChunkZ':
+        #             ChunkVolZ=float(line.split()[1])
+        #         if line.split()[0]=='vShear':
+        #             vwall=float(line.split()[1])
+
+        total_box_height, gapheight, ChunkArea, ChunkVolX, ChunkVolZ, vwall = 1,1,1,1,1,1
 
         # Unit conversion
         atm_to_mpa=101325*1e-6
@@ -244,6 +246,11 @@ class profile:
             var=time_avg_of_all_chunks[:,1]
             if inputfile == 'momentaX.profile':
                 var=var*gramA_per_fs_to_gram_per_m2_ns*3652
+
+            if inputfile == 'profile.gk.2d':
+                self.var=var
+
+            self.time = time
 
             np.savetxt(filename+'-time.txt', np.c_[time,var],
                         delimiter="  ",header="time           var" )
