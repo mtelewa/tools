e68f700eaf7d4c7969eaac08fe8b268595d7a0d5
mtelewa <mohamed.hassan@kit.edu>
2022-12-20 14:21:51 +0100

    fixed the remove command to remove only if file exists

--- snippets/extract_thermo.py
+++ snippets/extract_thermo.py
@@ -3,18 +3,22 @@
 
 import os
 import numpy as np
+from os.path import exists
 
 def extract_data(logfile, skip):
-    os.system(f"rm {os.path.dirname(logfile)}/thermo*.out")
-    os.system(f"cat {os.path.dirname(logfile)}/log.lammps | sed -n '/Step/,/Loop time/p' \
-    | head -n-1 > {os.path.dirname(logfile)}/thermo.out")
-    with open(f'{os.path.dirname(logfile)}/thermo.out', 'r') as f:
+    logfile_dir = os.path.dirname(logfile)
+
+    if exists(f"{logfile_dir}/thermo*.out"): os.system(f"rm {logfile_dir}/thermo*.out")
+    
+    os.system(f"cat {logfile_dir}/log.lammps | sed -n '/Step/,/Loop time/p' \
+    | head -n-1 > {logfile_dir}/thermo.out")
+    with open(f'{logfile_dir}/thermo.out', 'r') as f:
         for line in f:
             if line.split()[0]!='Step' and line.split()[0]!='Loop':
-                with open(f"{os.path.dirname(logfile) + '/thermo2.out'}", "a") as o:
+                with open(f"{logfile_dir + '/thermo2.out'}", "a") as o:
                     o.write(line)
             if line.split()[0]=='Step':
                 thermo_variables = line.split()
-    data = np.loadtxt(f"{os.path.dirname(logfile) + '/thermo2.out'}", skiprows=skip, dtype=float)
+    data = np.loadtxt(f"{logfile_dir + '/thermo2.out'}", skiprows=skip, dtype=float)
 
     return data, thermo_variables
