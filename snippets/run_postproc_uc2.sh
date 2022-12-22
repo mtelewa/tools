ab43c4c40099f6b23eef9939b8e05f873ef517d9
mtelewa <mohamed.hassan@kit.edu>
2022-12-22 18:48:01 +0100

    corrected the path to the post-processing submission script on UC2

--- snippets/run_postproc_uc2.sh
+++ snippets/run_postproc_uc2.sh
@@ -5,7 +5,7 @@ for i in $(ls -d */);do
 	#if [[ $i == *"$SUB"* ]] ; then
 	cd $i/data/out
 	  rm cluster.* flow_*
-		cp $HOME/tools/submit_scripts/submit_postproc.sh .
+		cp $HOME/tools/cluster/submit_postproc.sh .
 		sbatch submit_postproc.sh -i flow -N 144 -f pentane -s 0.4 -e 0.8 -p 0.0 -x 0.2
 	cd ../../../
 	#fi
