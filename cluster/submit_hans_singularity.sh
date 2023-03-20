791613e5e9e19acf25d5ef3c7ba541f68284babb
mtelewa <mohamed.hassan@kit.edu>
2023-03-20 12:41:59 +0100

    changed the workspace name on UC2

--- cluster/submit_hans_singularity.sh
+++ cluster/submit_hans_singularity.sh
@@ -14,4 +14,4 @@ export KMP_AFFINITY=compact,1,0
 module load compiler/intel/19.1
 module load mpi/openmpi/4.0
 
-mpirun --bind-to core --map-by core singularity exec --bind /scratch --bind /tmp --bind /pfs/work7/workspace/scratch/lr1762-flow_sims --pwd=$PWD $HOME/programs/hans.sif python3 -m hans -i $(pwd)/channel1D_DH.yaml
+mpirun --bind-to core --map-by core singularity exec --bind /scratch --bind /tmp --bind /pfs/work7/workspace/scratch/lr1762-flow --pwd=$PWD $HOME/programs/hans.sif python3 -m hans -i $(pwd)/channel1D_DH.yaml
